5dc66fd49193d75acc2f209cfc1a6cff0aad8027
Charntent <752293795@qq.com>
2018-05-23 21:43:20 +0800

    V1.0.3
    V1.0.3

---
+++ public/static/datas/nav.js
@@ -0,0 +1,76 @@
+var navs = [
+    {
+    "title": "网站管理",
+    "icon": "fa-cogs",
+    "spread": false,
+    "children": [
+        {
+            "title": "我的网站",
+            "icon": "fa-send",
+            "href": "/thome/sites"
+        }, {
+            "title": "新建网站",
+            "icon": "fa-send",
+            "href": "/thome/sites/add"
+        }, {
+            "title": "组件超市",
+            "icon": "fa-send",
+            "href": "/compnent"
+        }, {
+            "title": "免费模板",
+            "icon": "fa-send",
+            "href": "/case"
+        }, {
+            "title": "优秀案例",
+            "icon": "fa-send",
+            "href": "/case"
+        }
+
+    ]
+   }
+   ,{
+      "title": "财务管理",
+        "icon": "fa-cogs",
+        "spread": false,
+        "children": [
+             {
+                "title": "我的余额",
+                "icon": "fa-send",
+                "href": "/home/agents"
+            },
+            {
+                "title": "资金明细",
+                "icon": "fa-send",
+                "href": "/home/user"
+            }, {
+                "title": "充值提现",
+                "icon": "fa-send",
+                "href": "/home/namevalidate"
+            }
+
+            , {
+                "title": "收益管理",
+                "icon": "fa-send",
+                "href": "/home/creditlogs/index"
+            }
+        ]
+    }, {
+        "title": "开发者",
+        "icon": "fa-cogs",
+        "spread": false,
+        "children": [
+
+            {
+                "title": "我的身段",
+                "icon": "fa-send",
+                "href": "/home/order?status=0"
+            },
+            {
+                "title": "申请成为开发者",
+                "icon": "fa-send",
+                "href": "/home/order?status=1"
+            }
+        ]
+    }
+
+];
